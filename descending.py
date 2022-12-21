f175f21643f6fbbc0ac9ea0d6b2ca0c2f8fbef55
sneha-1003 <100294436+sneha-1003@users.noreply.github.com>
2022-12-21 14:59:06 +0530

    Create descending.py

---
+++ descending.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  4 21:30:54 2022
+@author: janaki
+"""
+
+n=100000
+fp = open("descending_"+str(n)+".txt","w")
+fp.write(str(n)+" ")  
+for i in range(n,1,-1):
+    fp.write(str(i)+" ")  
+fp.write(str(1))
+fp.close()
